0a04ccb30e8a02825eed9850c77fb4e5fdb6aa2f
Artem <Artem.dudarev2003@gmail.com>
2024-05-13 17:31:41 +0300

    Four classes done in the sixth homework

--- src/main/java/Fourth/MyList.java
+++ src/main/java/Fourth/MyList.java
@@ -20,18 +20,20 @@ public class MyList<T> {
     }
 
     public void addAll(T[] item){
-        if(counter == array.length-1){
-            Object[] newArray = new Object[size * 2];
-            System.arraycopy(array, 0 , newArray, 0, counter);
-            array = newArray;
+//        if(counter == array.length-1){
+//            Object[] newArray = new Object[size * 2];
+//            System.arraycopy(array, 0 , newArray, 0, counter);
+//            array = newArray;
+//        }
+//        for (int i = counter; i < array.length + counter; i++){
+//            for (int j = 0; j < item.length-1; j++){
+//                array[i] = item[j];
+//               // array[counter++] = item;
+//            }
+//        };
+        for (T items: item){
+            add(items);
         }
-        for (int i = counter; i < array.length + counter; i++){
-            for (int j = 0; j < item.length-1; j++){
-                array[i] = item[j];
-               // array[counter++] = item;
-            }
-        }
-
     }
 
     /*
@@ -41,23 +43,14 @@ public class MyList<T> {
         return (T) array[index];
     }
 
-    /*
-    Удаляет элемент списка по индексу. Все элементы справа от удаляемого
-    перемещаются на шаг налево. Если после удаления элемента количество
-    элементов стало в CUT_RATE раз меньше чем размер внутреннего массива,
-    то внутренний массив уменьшается в два раза, для экономии занимаемого
-    места.
-    */
+
     public void remove(int index) {
         for (int i = index; i< counter; i++)
             array[i] = array[i+1];
         array[counter] = null;
         counter--;
-//        if (array.length > INIT_SIZE && counter < array.length / CUT_RATE)
-//            resize(array.length/2); // если элементов в CUT_RATE раз меньше чем
-//        // длина массива, то уменьшу в два раза
     }
-    /*Возвращает количество элементов в списке*/
+
     public int size() {
         return counter;
     }
