8f824115bb24a4393bf923989011fdd9fa01b196
Artem <Artem.dudarev2003@gmail.com>
2024-05-20 16:42:50 +0300

    Some changes in master

--- src/main/java/FolderForMyself1/GitTasting.java
+++ src/main/java/FolderForMyself1/GitTasting.java
@@ -1,2 +1,7 @@
-package FolderForMyself1;public class GitTasting {
+package FolderForMyself1;
+
+public class GitTasting {
+    public static void main(String[] args) {
+        System.out.println("Тестирования git в главной ветке");
+    }
 }
