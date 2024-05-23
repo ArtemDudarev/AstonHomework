5c0d5e1b93e06379b4f02e2978151db6a59fea27
Artem <Artem.dudarev2003@gmail.com>
2024-05-23 17:50:35 +0300

    Test

--- src/main/java/FolderForTestingGit1/GitTesting.java
+++ src/main/java/FolderForTestingGit1/GitTesting.java
@@ -1,10 +1,8 @@
 package FolderForTestingGit1;
 
 
-public class GitTasting {
+public class GitTesting {
     public void someMethodOfMaster(){}
-
-
     public static void main(String[] args) {
         System.out.println("Тестирования git в главной ветке");
         System.out.println("Добавил новую ветку  workWithNIO и имитирую работу в ветке master");
