2ba9365b1ff03c9c4c41f1a8cdba82fb4e3d19f8
Artem <Artem.dudarev2003@gmail.com>
2024-05-23 16:56:58 +0300

    Imitation of work in master's branch

--- src/main/java/FolderForMyself1/GitTasting.java
+++ src/main/java/FolderForMyself1/GitTasting.java
@@ -3,5 +3,6 @@ package FolderForMyself1;
 public class GitTasting {
     public static void main(String[] args) {
         System.out.println("Тестирования git в главной ветке");
+        System.out.println("Добавил новую ветку  workWithNIO и имитирую работу в ветке master");
     }
 }
